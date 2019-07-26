f81dceebeadabf5e461510737316d6e07e62df5c
Muralidharan Ganapathy <43808822+MuralidharanGanapathy@users.noreply.github.com>
2019-07-26 10:45:15 +0530

    Update balancingstars.py

--- balancingstars.py
+++ balancingstars.py
@@ -34,12 +34,15 @@ for i in range(len(ip)):
     if ip[i] in ["]",")","}"] and open_is_there == 0:
         answer = "NO"
         continue
-    if ip[i] in [")","]","}"] and open_is_there == 1 and validate(ip[i],stack[top]) == True and i - top >= 2:
+    if ip[i] in [")","]","}"] and open_is_there == 1 and validate(ip[i],stack[top]) == True and len(stack)- top > 2:
         del stack[top:len(stack)]
         top = rev_iter(stack)
         balance_counter += 1
-    else:
-        continue
+    elif ip[i] in [")","]","}"]:
+        if open_is_there == 0 or validate(ip[i],stack[top]) == False or len(stack)- top < 2:
+            del stack[top:len(stack)]
+            top = rev_iter(stack)
+            continue
 if len(stack) == 0 and open_is_there == 1 and flag == 0:
     answer = "YES"
 else:
